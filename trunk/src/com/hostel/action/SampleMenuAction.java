bdb7e98bc425ea16893e98d25e81cc2de8e6724a
rajlaxmaniitg@gmail.com <rajlaxmaniitg@gmail.com@cff97035-4bd7-862b-3979-5de548b0d9d9>
2013-11-13 09:13:12 +0000

    Complete-POC : Struts2 + Tiles Integration

---
+++ trunk/src/com/hostel/action/SampleMenuAction.java
@@ -0,0 +1,12 @@
+package com.hostel.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SampleMenuAction extends ActionSupport{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public String tiger() { return "tiger"; }
+	public String lion() { return "lion"; }
+
+}
